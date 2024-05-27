0b85975734b1ac83f6720803090cf3f0ac2d4307
hazem mohamed <hazemm7622@gmail.com>
2024-05-27 02:23:54 +0200

    configure backend service with frontend

--- index.js
+++ index.js
@@ -1,45 +1,52 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "http://localhost:3030", // Your React frontend URL
+    methods: ["GET", "POST"]
+  }
+});
 const mongoose = require('mongoose');
 const multer = require('multer');
 const Tesseract = require('tesseract.js');
+const cors = require('cors');
+
+// Enable CORS
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
-})
+});
 
 const Mongo_db = "mongodb+srv://hazem:1234@cluster0.7enpft6.mongodb.net/";
 
 mongoose.connect(Mongo_db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((conn) => {
-        console.log(`database connected ${conn.connection.host}`)
+        console.log(`database connected ${conn.connection.host}`);
     }).catch((err) => {
-        console.log(err)
-    })
+        console.log(err);
+    });
 
 const MessagesSchema = new mongoose.Schema({
     Author: String,
     Content: String,
     image: String
-})
+});
 
 const Messages = mongoose.model('Messages', MessagesSchema);
 
 io.on('connection', (socket) => {
-    console.log('new client is connecting')
+    console.log('new client is connecting');
 
     socket.on('username', (username) => {
-        console.log("the logged username is" + "" + username)
-        socket.username = username
-        io.emit("userJoined", username)
-        socket.username = username
-    })
+        console.log("the logged username is " + username);
+        socket.username = username;
+        io.emit("userJoined", username);
+    });
 
     socket.on('chat message', async (msg) => {
         if (msg.image) {
-            // Use Tesseract.js to extract text from the image
             try {
                 const result = await Tesseract.recognize(
                     msg.image,
@@ -51,7 +58,6 @@ io.on('connection', (socket) => {
                 console.error('Error extracting text from image:', error);
             }
         }
-        socket.username = msg.username
         const message = new Messages({
             Author: msg.Author,
             Content: msg.Content,
@@ -60,21 +66,21 @@ io.on('connection', (socket) => {
 
         message.save()
             .then(() => {
-                io.emit("chat message", msg)
+                io.emit("chat message", msg);
             })
             .catch((err) => {
-                console.log(err)
-            })
-    })
+                console.log(err);
+            });
+    });
 
     socket.on('disconnect', () => {
-        console.log('user disconnected')
-        io.emit("user left", socket.username)
-    })
-})
+        console.log('user disconnected');
+        io.emit("user left", socket.username);
+    });
+});
 
 app.use(express.static('public'));
 
 http.listen(5000, () => {
-    console.log('listening on *:5000')
-})
+    console.log('listening on *:5000');
+});
